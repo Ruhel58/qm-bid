a422aea1ff624f5686681e424e26eb874706c6d2
Ruhel <ruhel@ruhels-mbp.wkstn.qmul.ac.uk>
2019-11-28 22:16:45 +0000

    settings config

--- project/settings.py
+++ project/settings.py
@@ -74,7 +74,7 @@ TEMPLATES = [
     },
 ]
 
-#WSGI_APPLICATION = 'wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
